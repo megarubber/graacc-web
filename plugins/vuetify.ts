e0db5c4baad3a456cc95ab1eab2680b73fa1ec21
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2024-08-30 17:33:45 -0300

    feat: add custom theme

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,9 +1,25 @@
 import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
+
+const graacc_light: ThemeDefinition = {
+    dark: false,
+    colors: {
+        'blue-light': '#ABD8FB',
+        'blue-dark': '#1976D2',
+        'green': '#4CAF50',
+    }
+}
 
 export default defineNuxtPlugin((app) => {
-    const vuetify = createVuetify()
+    const vuetify = createVuetify({
+        theme: {
+            defaultTheme: 'graacc_light',
+            themes: {
+                graacc_light,
+            },
+        },
+    })
     app.vueApp.use(vuetify)
 })
