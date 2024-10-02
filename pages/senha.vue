a68de405f044e0d4368c4eb39f937a1f1d88a84b
Guilherme Samuel <guisamuel800@gmail.com>
2024-10-02 15:54:59 -0300

    feat: add recover password page

---
+++ pages/senha.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-layout>
+        <v-container>
+            <v-icon icon="mdi-lock" size="x-large"></v-icon>
+            <h1 class="text-left mb-4 font-weight-bold">Esqueceu sua senha?</h1>
+            <p class="mb-4">Digite o e-mail associado à sua conta</p>
+            <div class="d-flex flex-column ga-4">
+                <v-text-field 
+                rounded="lg" 
+                label="E-mail"
+                variant="outlined"></v-text-field>
+                <div class="d-flex flex-row">
+                    <p class="mb-4 mr-2">Lembrou sua senha?</p>
+                    <a href="#" class="font-weight-bold">Faça login</a>
+                </div>
+                <v-btn
+                variant="flat"
+                color="blue-dark"
+                rounded="lg">Enviar pedido de alteração</v-btn>
+            </div>
+        </v-container>
+    </v-layout>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+    name: 'EncontrarSenhaPage',
+})
+</script>
+<style>
+a { 
+    color: inherit; 
+    text-decoration: none;
+    color:#007AFF;
+} 
+</style>
