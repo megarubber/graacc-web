74263e482499f5d25963421d9551e6cc16196265
Guilherme Samuel <guisamuel800@gmail.com>
2024-10-16 18:07:23 -0300

    feat: create base calendar screen

---
+++ pages/calendario.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout>
+      <v-container>
+        <v-calendar />
+      </v-container>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { VCalendar } from 'vuetify/labs/VCalendar'
+
+export default defineComponent({
+  name: 'Calendário',
+  components: {
+    VCalendar,
+  },
+})
+</script>
+
+<style>
+
+</style>
