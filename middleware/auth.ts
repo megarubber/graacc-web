a708044fda783626e6408f1bb33b5278faa8ed4a
CaioAbe02 <66100834+CaioAbe02@users.noreply.github.com>
2024-11-05 10:44:53 -0300

    feat: auth middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,15 @@
+import { useAuthStore } from "~/store/auth"
+
+export default defineNuxtRouteMiddleware((to) => {
+  const { authenticated } = storeToRefs(useAuthStore())
+  const token = useCookie('token')
+
+  if (token.value) {
+    authenticated.value = true
+  }
+
+  if (!token.value && to?.name !== 'login') {
+    abortNavigation()
+    return navigateTo('/login')
+  }
+})
