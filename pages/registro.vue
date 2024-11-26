ec7a6139f6a3d36603cac5403e9832f9a3260790
Guilherme Samuel <guisamuel800@gmail.com>
2024-11-26 10:52:59 -0300

    feat: fetch create users when button is pressed

--- pages/registro.vue
+++ pages/registro.vue
@@ -7,35 +7,40 @@
                 <v-text-field 
                 rounded="lg" 
                 label="Nome"
+                v-model="user.nome"
                 placeholder="José Maria da Silva"
                 variant="outlined"></v-text-field>
                 <v-text-field 
                 label="E-mail"
                 rounded="lg"
                 placeholder="exemplo@gmail.com"
+                v-model="user.email"
                 color="blue-dark"
                 variant="outlined"></v-text-field>
                 <v-text-field 
                 label="Senha"
                 rounded="lg"
                 placeholder="Crie a sua senha"
+                v-model="user.senha"
                 color="blue-dark"
-				:type="show ? 'text' : 'password'"
-				:append-icon="show ?'mdi-eye': 'mdi-eye-off'"
-				@click:append="show = !show"
+                :type="show ? 'text' : 'password'"
+                :append-icon="show ?'mdi-eye': 'mdi-eye-off'"
+                @click:append="show = !show"
                 variant="outlined"></v-text-field>
                 <v-text-field 
                 label="Confirme a sua senha"
                 rounded="lg"
                 placeholder="Confirme a sua senha"
+                v-model="user.confirme_senha"
                 color="blue-dark"
-				:type="show_confirm ? 'text' : 'password'"
-				:append-icon="show_confirm ?'mdi-eye':'mdi-eye-off'"
-				@click:append="show_confirm = !show_confirm"
+                :type="show_confirm ? 'text' : 'password'"
+                :append-icon="show_confirm ?'mdi-eye':'mdi-eye-off'"
+                @click:append="show_confirm = !show_confirm"
                 variant="outlined"></v-text-field>
                 <v-btn
                 variant="flat"
                 color="blue-dark"
+                @click="register()"
                 rounded="lg">Enviar</v-btn>
             </div>
         </v-container>
@@ -43,17 +48,42 @@
 </template>
 
 <script lang="ts">
+import createUser from '~/utils/api/register/createUser'
+
 export default defineComponent({
-    name: 'Registro',
+  name: 'Registro',
 	data() {
-        return {
-            show: false,
-            show_confirm: false
-        }
-	}
+    return {
+      user: {
+          nome: '',
+          email: '',
+          senha: '',
+          confirme_senha: ''
+      },
+      show: false,
+      show_confirm: false,
+      alert: false,
+      alert_message: '',
+    }
+  },
+  methods: {
+    async register() {
+      if(this.user.senha != this.user.confirme_senha) {
+        this.alert = true
+        this.alert_message = 'Senhas estão diferentes'
+        return
+      }
+
+      try { 
+        const userRegister = await createUser(this.user)
+        if (userRegister) this.$router.push('/login')
+      }
+      catch (error: any) {
+        this.alert = true
+        this.alert_message = error.response._data.mensagem
+      }
+    }
+  }
 });
 </script>
 
-<style>
-
-</style>
