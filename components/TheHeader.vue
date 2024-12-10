c91bad942d6face2591b54e857449a3f5be3a29e
BrunoKenji2 <bruno.kenji21@gmail.com>
2024-12-10 20:30:41 -0300

    adding icons

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,17 +2,29 @@
     <v-layout>
         <v-container>
             <v-tabs>
-                <v-tab to="/">
-                    Exames
+                <v-tab to="/exames">
+                    <div class="custom-tab">
+                        <v-icon size= "24">mdi-home</v-icon>
+                            <div>Exames</div>
+                    </div>
                 </v-tab>
                 <v-tab to="/calendario">
-                    Calendário
+                    <div class="custom-tab">
+                        <v-icon size ="24">mdi-calendar</v-icon>
+                        <div>Calendário</div>
+                    </div>
                 </v-tab>
                 <v-tab to="/contatos">
-                    Contatos
+                    <div class="custom-tab">
+                        <v-icon size= "24">mdi-contacts</v-icon>
+                            <div>Contatos</div>
+                    </div>
                 </v-tab>
                 <v-tab to="/notificacoes">
-                    Notificações
+                    <div class="custom-tab">
+                        <v-icon size ="24">{{hasNotifications ? 'mdi-bell-ring' : 'mdi-bell'}}</v-icon>
+                        <div>Notificações</div>
+                    </div>
                 </v-tab>
             </v-tabs>
         </v-container>
@@ -23,7 +35,29 @@
     import { defineComponent } from 'vue';
 
     export default defineComponent({
-        name: 'TheHeader'
-    });
+        name: 'TheHeader',
+        setup() {
+            // simulação icone de notificações
+            const hasNotifications = ref(false);
 
+            // a cada 5 segundos muda o estado da notificação
+            setTimeout(() => {
+                hasNotifications.value = !hasNotifications.value;
+            }, 2000);
+
+            return {hasNotifications};
+        },
+    });
 </script>
+
+
+<style>
+.custom-tab {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-transform: none;
+    gap: 4px;
+}
+</style>
