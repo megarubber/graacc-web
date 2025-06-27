c51ad451cb6671c2bd838f5c848337e3eb3ba39c
Guilherme Samuel <guisamuel800@gmail.com>
2025-06-27 20:47:26 -0300

    refactor(css, style): update buttons and logo of login screen

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-layout>
-        <v-container>
+    <v-layout class="h-100">
+        <v-container class="d-flex flex-column justify-center">
             <v-snackbar
                 v-model="alert"
                 location="top end"
@@ -8,7 +8,9 @@
             >
                 {{ alert_message }}
             </v-snackbar>
-            <h1 class="text-center mb-4 font-weight-bold">Bem-vindo!</h1>
+            <img src="/assets/images/agendinha_logo.png" class="align-self-center"/>
+            <h2 class="text-center font-weight-bold">Vamos começar?</h2>
+            <h3 class="text-center mb-6">Faça seu login</h3>
             <div class="d-flex flex-column ga-4">
                 <v-text-field
                 v-model="user.email"
@@ -19,31 +21,27 @@
                 v-model="user.senha"
                 label="Senha"
                 rounded="lg"
-                color="blue-dark"
                 :type="show ? 'text' : 'password'"
-				:append-icon="show ?'mdi-eye': 'mdi-eye-off'"
-				@click:append="show = !show"
+                :append-inner-icon="show ?'mdi-eye': 'mdi-eye-off'"
+                @click:append-inner="show = !show"
                 variant="outlined"></v-text-field>
-                <a href="/senha" class="font-weight-bold text-primary">Esqueceu a senha?</a>
+                <a href="/senha" class="font-weight-bold mb-4 text-blue-light">Esqueceu a senha?</a>
                 <v-btn
                 variant="flat"
-                color="blue-dark"
-                rounded="lg"
-                @click="login()">Login</v-btn>
+                @click="login()">Entrar</v-btn>
+                <p class="text-center">ou</p>
                 <v-btn
-                variant="flat"
-                color="blue-dark"
-                rounded="lg"
-                to="/registro">Cadastre-se</v-btn>
-                <v-btn
-                color="grey"
-                variant="flat">
-                    <v-icon
-                    icon="mdi-google"
-                    color="pink"
-                    start></v-icon>
-					Entrar com o Google
+                color="black"
+                variant="outlined">
+                  <Icon style="margin-right: 10px;" name="icons:google-logo" size="30"/>
+					        Entrar com o Google
                 </v-btn>
+                <div class="text-center mt-6">
+                  <p>Ainda não tem uma conta?</p>
+                  <a href="/registro" class="font-weight-bold mb-6 text-blue-light">
+                    Cadastre-se aqui.
+                  </a>
+                </div>
             </div>
         </v-container>
     </v-layout>
@@ -91,4 +89,7 @@ a {
     text-decoration: none;
     color:#007AFF;
 } 
+img {
+  width: 200px;
+}
 </style>
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,12 +1,13 @@
 import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
+
 import { createVuetify, type ThemeDefinition } from 'vuetify'
 
 const graacc_light: ThemeDefinition = {
     dark: false,
     colors: {
-        'blue-light': '#ABD8FB',
+        'blue-light': '#1C3B72',
         'blue-dark': '#1976D2',
         'green': '#4CAF50',
         'grey': '#F5F5F5',
@@ -22,6 +23,17 @@ export default defineNuxtPlugin((app) => {
                 graacc_light,
             },
         },
-    })
+        defaults: {
+          global: { ripple: false },
+          VTextField: { variant: 'outlined' },
+          VBtn: {
+            color: '#1C3B72',
+            class: ['font-weight-regular', 'text-none'],
+            style: [ { 'letterSpacing': 'normal' } ],
+            rounded: 'xl',
+            size: 'x-large',
+          },
+        },
+     })
     app.vueApp.use(vuetify)
 })
