ee7392998fb671e85d0ec701edb875d5899f624e
Guilherme Samuel <guisamuel800@gmail.com>
2025-07-02 12:51:08 -0300

    refactor: update UI of send password request screen

--- pages/senha.vue
+++ pages/senha.vue
@@ -1,23 +1,17 @@
 <template>
-    <v-layout>
-        <v-container>
-            <v-icon icon="mdi-lock" size="x-large"></v-icon>
-            <h1 class="text-left mb-4 font-weight-bold">Esqueceu sua senha?</h1>
-            <p class="mb-4">Digite o e-mail associado à sua conta</p>
-            <div class="d-flex flex-column ga-4">
-                <v-text-field 
-                rounded="lg" 
-                label="E-mail"
-                variant="outlined"></v-text-field>
-                <div class="d-flex flex-row">
-                    <p class="mb-4 mr-2">Lembrou sua senha?</p>
-                    <a href="/login" class="font-weight-bold">Faça login</a>
-                </div>
-                <v-btn
-                variant="flat"
-                color="blue-dark"
-                rounded="lg">Enviar pedido de alteração</v-btn>
-            </div>
+    <v-layout class="h-100">
+        <v-container class="d-flex flex-column justify-center align-center">
+          <v-icon icon="mdi-lock" size="x-large"></v-icon>
+          <h1 class="text-center mb-4 font-weight-bold">Esqueceu sua senha?</h1>
+          <section class="w-100">
+            <p class="mb-4 text-center">Digite o e-mail associado à sua conta</p>
+            <v-text-field label="E-mail"></v-text-field>
+          </section>
+          <div class="d-flex flex-row">
+              <p class="mb-4 mr-2">Lembrou sua senha?</p>
+              <a href="/login" class="font-weight-bold mb-6 text-blue-light">Faça login</a>
+          </div>
+          <v-btn>Enviar pedido de alteração</v-btn>
         </v-container>
     </v-layout>
 </template>
@@ -38,10 +32,9 @@ export default defineComponent({
     },
 })
 </script>
-<style>
+<style scoped>
 a { 
     color: inherit; 
     text-decoration: none;
-    color:#007AFF;
 } 
 </style>
