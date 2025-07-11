b15351341bd98eebebfd31713203886dafa3b84b
Guilherme Samuel <guisamuel800@gmail.com>
2025-07-11 01:30:07 -0300

    feat: create fully user profile screen

--- pages/perfil.vue
+++ pages/perfil.vue
@@ -1,2 +1,115 @@
 <template>
+  <v-layout class="h-100 d-flex flex-column justify-center align-center">
+    <div class="glow">
+      <div class="glow-effect l top-0"/>
+      <div class="glow-effect r t"/>
+    </div>
+    <v-progress-circular v-if="loading" indeterminate />
+    <v-container v-else class="text-center d-flex flex-column justify-center align-center ga-4">
+      <div class="image-container">
+        <img src="/assets/images/sample.jpg">
+      </div>
+      <h3>John Doe</h3>
+      <h3 style="color: #4B4B4B;">Responsável: {{ user.nome }}</h3>
+      <v-btn 
+        class="w-100" 
+        color="#F8F8F8" 
+        text="Atualizar dados" 
+        prepend-icon="mdi-pencil-outline"
+      />
+      <v-btn 
+        class="w-100" 
+        color="#F8F8F8" 
+        text="Preferências de notificações" 
+        prepend-icon="mdi-bell-outline"
+      />
+      <v-btn 
+        class="w-100" 
+        color="#F8F8F8" 
+        text="Desconectar" 
+        prepend-icon="mdi-exit-to-app"
+				@click="logUserOut()"
+				to="/login"
+      />
+			<TheHeader />
+    </v-container>
+  </v-layout>
 </template>
+
+<script lang="ts">
+import getUserInfo from '~/utils/api/user/getUserInfo'
+import { useAuthStore } from '~/store/auth'
+
+export default defineComponent({
+  name: 'Profile',
+  data() {
+    return {
+      user: ref({
+        nome: '',
+        email: '',
+      }),
+      auth: useAuthStore(),
+			loading: ref(false)
+    }
+  },
+  async mounted() {
+		this.loading = true
+    this.user = await getUserInfo()
+		this.loading = false
+  },
+  methods: {
+    logUserOut() {
+			try {
+				this.loading = true
+				this.auth.logUserOut()
+			} catch(e) {
+				console.error(e)
+				this.loading = false
+			}
+    }
+  }
+});
+</script>
+
+<style scoped>
+.glow-effect {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  opacity: 15%;
+  z-index: -1;
+  box-shadow:
+    0 0 100px 5rem #80D9FF,
+    0 0 140px 6rem #009EE0;
+}
+
+.glow-effect.r.t {
+  top: 0px;
+  box-shadow:
+    0 0 100px 5rem #E32585,
+    0 0 140px 6rem #E858A1;
+}
+
+.glow-effect.r { right: 5px; }
+
+.glow-effect.l { left: 5px; }
+
+.v-btn {
+  justify-content: flex-start;
+  text-align: left;
+}
+
+.image-container {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.image-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+</style>
